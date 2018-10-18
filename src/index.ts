d0a6d434cd70b88384e4211c4b3c8eedbdf121d8
ButteryCrumpet <crumpetybumpety@gmail.com>
2018-10-18 15:08:34 +0900

    0.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,28 @@
-import { BasicPost } from "./wp-api/post"
-import { PostQueryArgs, TermQueryArgs } from "./wp-api/query"
-import { Term } from "./wp-api/term"
-import { PostQuery, Query, TermQuery } from "./wp-api/wp-api"
+import { BasicPost } from "./wp-api/post";
+import { PostQueryArgs, TermQueryArgs } from "./wp-api/query";
+import { Term } from "./wp-api/term";
+import { PostQuery, Query, TermQuery } from "./wp-api/wp-api";
 
 export class WPClient {
-  public getPosts: Query<BasicPost[], PostQueryArgs>
-  public getTerms: Query<Term[], TermQueryArgs>
+  public getPosts: Query<BasicPost[], PostQueryArgs>;
+  public getTerms: Query<Term[], TermQueryArgs>;
 
-  private base: string
+  private base: string;
   constructor(base: string) {
-    this.base = base
-    this.getPosts = PostQuery(base)
-    this.getTerms = TermQuery(base)
-  } 
+    this.base = base;
+    this.getPosts = PostQuery(base);
+    this.getTerms = TermQuery(base);
+  }
 }
 
-export { ErrorType } from "./wp-api/wp-api"
-export { Result, isError, isOk, unwrap } from "./wp-api/result"
-export { BasicPost } from "./wp-api/post"
-export { EmbedTerm, Term } from "./wp-api/term"
-export { Order, PostOrderBy, PostQueryArgs, TermQueryArgs } from "./wp-api/query"
-export default WPClient
+export { ErrorType } from "./wp-api/wp-api";
+export { Result, isError, isOk, unwrap } from "./wp-api/result";
+export { BasicPost } from "./wp-api/post";
+export { EmbedTerm, Term } from "./wp-api/term";
+export {
+  Order,
+  PostOrderBy,
+  PostQueryArgs,
+  TermQueryArgs,
+} from "./wp-api/query";
+export default WPClient;
